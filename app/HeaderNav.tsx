3f994148d8164c9bfb3244fddadd69ab2f743795
kanishk99 <kanishkkargutkar123@gmail.com>
2023-03-28 01:13:06 +0530

    installed dependencies and base

---
+++ app/HeaderNav.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { useState } from "react";
+
+export default function HeaderNav(){
+    // const[auth,setAuth] = useState(false);
+
+    // useEffect(()=>{
+    //     onAuthChanged(user=>{
+    //         setAuth(!!user);
+    //     })
+    // },[])
+
+    return <nav>
+        {/* {!auth && <Link href="/login">Login</Link>}
+        {auth && <Link href="/dashboard">Dashboard</Link>} */}
+    </nav>
+}
